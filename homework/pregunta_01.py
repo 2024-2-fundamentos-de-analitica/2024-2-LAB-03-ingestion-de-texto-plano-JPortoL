cdad6ef30a98e0ad03ee8f1ebbcfe024ab98800b
Jesús Miguel Porto López <jporto@unal.edu.co>
2025-02-03 00:32:39 -0500

    v1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,7 +3,70 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
 # pylint: disable=import-outside-toplevel
+import pandas as pd
+import re
 
+
+def limpiar_encabezado(dir):
+    with(open(dir, "r")) as file:
+        lineas = file.readlines()
+
+    linea1 = re.split(r'(?=[A-Z])', lineas[0].strip())
+    linea2 = re.split(r'(?=[p])', lineas[1].strip())
+
+    linea1 = [
+    re.sub(r'\s+', '_', item.strip().lower())  
+    for item in linea1 if item 
+    ]
+
+    linea2 = [
+    re.sub(r'\s+', '_', item.strip().lower()) 
+    for item in linea2 if item 
+    ]
+
+    encabezados = [
+      linea1[0],
+      linea1[1] + '_' + linea2[0],
+      linea1[2] + '_' + linea2[1],
+      linea1[3],
+    ]
+
+    #coñoetumadreculemondalargacolejoaaaaa
+    return encabezados
+
+def limpiar_cuerpo(dir):
+    with(open(dir, "r")) as file:
+        lineas = file.readlines()
+    
+    filas = []
+    fila_actual = []
+    palabras = ""
+    for linea in lineas[4:]:
+        if re.match(r"^\s+\d+", linea):
+            if fila_actual:
+                fila_actual.append(palabras[1:])
+                filas.append(fila_actual)
+            fila_actual = []
+            partes = re.split(r'\s+', linea.strip())
+            cluster = int(partes[0])
+            cantidad = int(partes[1])
+            porcentaje = float(partes[2].replace(',', '.'))
+            fila_actual = [cluster, cantidad, porcentaje]
+            palabras =  re.sub(r'\s{2,}', '', " ".join(partes[3:])).replace('%', '').replace('.', '')
+        else:
+            palabras += " " + re.sub(r'\s{2,}', ' ', linea.strip()).replace('%', '').replace('.', '')
+
+    # if fila_actual:
+    #     fila_actual.append(palabras[1:])
+    #     filas.append(fila_actual)
+    # for fila in filas:
+    #     print(fila, "\n \n")
+
+    # eldiavlolocoestoestamuyheavy
+    return filas
+
+            
+            
 
 def pregunta_01():
     """
@@ -18,3 +81,11 @@ def pregunta_01():
 
 
     """
+    encabezados = limpiar_encabezado('files/input/clusters_report.txt')
+    filas = limpiar_cuerpo('files/input/clusters_report.txt')
+    df = pd.DataFrame(filas, columns=encabezados)
+    # print(df.principales_palabras_clave.to_list()[1], "\n \n")
+    return df
+
+if __name__ == '__main__':
+    print(pregunta_01())
